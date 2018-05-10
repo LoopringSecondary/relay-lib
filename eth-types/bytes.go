3247e56de21e680fcc3ce3e247671e2918e92103
符坤 <fukun@fukundeMacBook-Pro.local>
2018-05-10 10:59:51 +0800

    add eth-abi and eth-types

---
+++ eth-types/bytes.go
@@ -0,0 +1,73 @@
+package types
+
+import (
+	"fmt"
+	"github.com/ethereum/go-ethereum/common"
+	"math/big"
+)
+
+const (
+	Bytes32Length = 32
+)
+
+type Bytes32 [Bytes32Length]byte
+
+func BitToBytes32(b *big.Int) Bytes32 { return BytesToBytes32(b.Bytes()[:]) }
+func HexToBytes32(s string) Bytes32   { return BytesToBytes32(common.FromHex(s)) }
+
+//MarshalJson
+func (a *Bytes32) MarshalText() ([]byte, error) {
+	return []byte(a.Hex()), nil
+}
+
+func (a *Bytes32) UnmarshalText(input []byte) error {
+	a.SetBytes(HexToBytes32(string(input)).Bytes())
+	return nil
+}
+
+func (s Bytes32) Str() string       { return string(s[:]) }
+func (s Bytes32) Bytes() []byte     { return s[:] }
+func (s Bytes32) Bytes32() [32]byte { return s }
+func (s Bytes32) Big() *big.Int     { return new(big.Int).SetBytes(s[:]) }
+func (s Bytes32) Hex() string       { return common.ToHex(s[:]) }
+
+func BytesToBytes32(b []byte) Bytes32 {
+	var s Bytes32
+	s.SetBytes(b)
+	return s
+}
+
+func (s *Bytes32) SetBytes(b []byte) {
+	if len(b) > len(s) {
+		b = b[len(b)-Bytes32Length:]
+	}
+	copy(s[Bytes32Length-len(b):], b)
+}
+
+func IsZeroHash(hash common.Hash) bool {
+	return hash == common.HexToHash("0x")
+}
+
+func IsZeroAddress(addr common.Address) bool {
+	return addr == common.HexToAddress("0x")
+}
+
+func BigintToHex(b *big.Int) string {
+	if nil == b {
+		b = big.NewInt(0)
+	}
+	return fmt.Sprintf("%#x", b)
+}
+
+func HexToBigint(h string) *big.Int {
+	return new(big.Int).SetBytes(common.FromHex(h))
+}
+
+type CheckNull interface {
+	IsNull() bool
+}
+
+var (
+	NilHash    = common.HexToHash("0x")
+	NilAddress = common.HexToAddress("0x")
+)
