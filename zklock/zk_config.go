59389b74727d1355193a18f739912f8162c271d9
liweichao <weichao092@163.com>
2018-06-15 15:19:18 +0800

    refactory zk_config for log info

--- zklock/zk_config.go
+++ zklock/zk_config.go
@@ -33,21 +33,21 @@ func RegisterConfigHandler(namespace string, key string, action HandlerFunc) err
 		return fmt.Errorf("zkClient is not intiliazed")
 	}
 	if _, err := CreatePath(configShareBasePath); err != nil {
-		return fmt.Errorf("create config base path failed %s", configShareBasePath)
+		return fmt.Errorf("create config base path failed %s with error %s", configShareBasePath, err.Error())
 	}
 	ns := fmt.Sprintf("%s/%s", configShareBasePath, namespace)
 	if _, err := CreatePath(ns); err != nil {
-		return fmt.Errorf("create config namespace path failed %s", ns)
+		return fmt.Errorf("create config namespace path failed %s with error %s", ns, err.Error())
 	}
 	keyPath := fmt.Sprintf("%s/%s", ns, key)
 	if _, err := CreatePath(keyPath); err != nil {
-		return fmt.Errorf("create config key path failed %s", keyPath)
+		return fmt.Errorf("create config key path failed %s with error %s", keyPath, err.Error())
 	}
 
 	if data, _, ch, err := ZkClient.GetW(keyPath); err != nil {
-		log.Errorf("Get config keyPath %s data failed with error : %s", keyPath, err.Error())
+		log.Errorf("Get config %s data failed with error : %s", keyPath, err.Error())
 	} else {
-		log.Infof("Get config keyPath %s data success", keyPath)
+		log.Infof("Get config %s data success", keyPath)
 		action(data)
 		go func() {
 			for {
@@ -55,9 +55,9 @@ func RegisterConfigHandler(namespace string, key string, action HandlerFunc) err
 				case evt := <- ch:
 					if evt.Type == zk.EventNodeDataChanged {
 						if data, _, chx, err := ZkClient.GetW(keyPath); err != nil {
-							log.Errorf("Get config keyPath %s data failed with error : %s", keyPath, err.Error())
+							log.Errorf("Get config %s data failed with error : %s", keyPath, err.Error())
 						} else {
-							log.Infof("config keyPath %s data changed !!", keyPath)
+							log.Infof("config %s data changed to value %s", keyPath, string(data[:]))
 							ch = chx
 							action(data)
 						}
